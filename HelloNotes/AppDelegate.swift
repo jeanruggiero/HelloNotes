adfd8a93fd214fc29ded0fbeebdaa75607f94119
Jean Ruggiero <jeanruggiero@gmail.com>
2020-01-08 18:35:06 -0500

    Initial Commit

---
+++ HelloNotes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HelloNotes
+//
+//  Created by Jean Ruggiero on 1/8/20.
+//  Copyright © 2020 Jean Ruggiero. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HelloNotes/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  HelloNotes
+//
+//  Created by Jean Ruggiero on 1/8/20.
+//  Copyright © 2020 Jean Ruggiero. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
